d9ce57dc08ad71f403e84a3d639027a5d658ca87
armi1189 <armi1189@gmail.com>
2015-04-22 10:20:53 +0100

    can send messages

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnect');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, function(){
